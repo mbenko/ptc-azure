f28b3d112cd6f8fda583634d7b79aecff2a3f0e8
Mike Benkovich <e9003505@ltfinc.net>
2021-11-30 09:55:29 -0600

    Add CI/CD deploy

--- Deploy/TF/main.tf
+++ Deploy/TF/main.tf
@@ -9,21 +9,43 @@ terraform {
   required_version = ">= 0.14.9"
 }
 
-provider "azurerm" { 
-    # The "feature" block is required for AzureRM provider 2.x. 
-    # If you are using version 1.x, the "features" block is not allowed.
-    # version = "~>2.0"
-    features {
-      key_vault {
-        purge_soft_delete_on_destroy = true
-      }
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x. 
+  # If you are using version 1.x, the "features" block is not allowed.
+  # version = "~>2.0"
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
     }
+  }
 }
+data "azurerm_client_config" "current" {}
+
+######################################################################
+##  VARS
+######################################################################
+variable "app_name" {
+  type    = string
+  default = "bnkApp"
+}
+
+variable "region" {
+  type    = string
+  default = "centralus"
+}
+
+variable "loc" {
+  type    = string
+  default = "cus"
+}
+
+######################################################################
+##  RESOURCES
+######################################################################
 
 resource "azurerm_resource_group" "rg" {
-  name      = "rg-dadapp-tf"
-  location = "centralus"
+  name     = "tf-${var.app_name}-${var.loc}"
+  location = var.region
 }
 
-data "azurerm_client_config" "current" {}
 
--- Deploy/TF/myKeyvault.tf
+++ Deploy/TF/myKeyvault.tf
@@ -1,5 +1,5 @@
 resource "azurerm_key_vault" "myKeyVault" {
-  name                        = "tf-dadapp-kv-v2"
+  name                        = "tf-${var.app_name}-kv"
   location                    = azurerm_resource_group.rg.location
   resource_group_name         = azurerm_resource_group.rg.name
   enabled_for_disk_encryption = true
@@ -19,7 +19,7 @@ resource "azurerm_key_vault" "myKeyVault" {
     ]
 
     secret_permissions = [
-      "Get","Set","List","Delete","Recover","Backup","Restore"
+      "Get", "Set", "List", "Delete", "Recover", "Backup", "Restore"
     ]
 
     storage_permissions = [
@@ -28,11 +28,10 @@ resource "azurerm_key_vault" "myKeyVault" {
   }
 }
 
-
-
+# Add my-secret
 resource "azurerm_key_vault_secret" "keyvault_secret" {
   name         = "my-secret"
-  value        = "Set from KeyVault!"
+  value        = "Set from KeyVault! Hello ${var.app_name}"
   key_vault_id = azurerm_key_vault.myKeyVault.id
   lifecycle {
     ignore_changes = [value, version]
